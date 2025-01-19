0198015a963588f34bbdfa0ce91b12687c0c54b7
Artur Gol <artur.golavskiy@metropolia.fi>
2025-01-20 01:16:38 +0200

    Initial react commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-blue-500 text-white text-3xl font-bold">
+      Tailwind CSS with React!
+    </div>
+  );
+}
+
+export default App;
