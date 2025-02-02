fa622d5ed95b28d890f1e8dd122fe932fcab78fa
Artur Gol <artur.golavskiy@metropolia.fi>
2025-02-03 01:51:05 +0200

    new component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Skeleton from "../components/Skeleton";
 import PageNotFound from "../components/PageNotFound";
+// import ComingSoon from "../components/ComingSoon";
 
 const Home = () => {
   return (
     <div className="bg-gray-900 min-h-screen flex flex-col">
       <PageNotFound>
-        Oops! This page is currently under development. Please check back later.
-        <p className="mt-2 text-sm text-gray-500">Last updated: 2/2025</p>
+        Oops! This website is currently under development. Please check back
+        later.
+        <p className="mt-6 text-sm text-gray-500">Last updated: 2/2025</p>
         <div className="gap-4 mt-8 flex-grow">
           <Skeleton />
         </div>
